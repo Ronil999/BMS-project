5616dd93b5d92643d6c2e2795caa4abea815ba1c
Ronil999 <104007767+Ronil999@users.noreply.github.com>
2023-01-23 00:27:51 +0530

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,40 @@
+class password{
+    constructor(uname,passwd){
+        this.uname = uname;
+        this.passwd = passwd;
+    }
+    checkpwdd(){
+        if(user == this.uname && pass == this.passwd){
+            // document.getElementById("err").innerHTML = "Successfully Login";
+            // console.log("login");
+            window.location.href = '/admin/admin.html';
+        }
+        else{
+            document.getElementById("err").innerHTML = "Wrong Password!  Try again!!";
+            // console.log("not login");
+        }
+    }
+};
+
+var user;
+var pass;
+
+function ru() {
+    user = document.getElementById("un").value;
+    console.log(user);
+}
+
+function rp() {
+    pass = document.getElementById("pwd").value;
+    console.log(pass);
+}
+
+// console.log(passwd);    
+
+const pd = new password("admin","admin");
+
+function checkpwd() {
+    
+    pd.checkpwdd();
+}
+
